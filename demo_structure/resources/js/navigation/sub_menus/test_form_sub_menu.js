bd2f8d82045781972278da50e2d1e2469f30d46e
j0hnys <contact_me@johnstamkos.com>
2018-12-14 22:00:56 +0200

    Update test_form_sub_menu.js

--- demo_structure/resources/js/navigation/sub_menus/test_form_sub_menu.js
+++ demo_structure/resources/js/navigation/sub_menus/test_form_sub_menu.js
@@ -3,43 +3,43 @@ const test_form_sub_menu = [
         name: '1',
         icon_type: 'ios-navigate',
         text: 'sub Option 1',
-        redirect_url: '/vista-framework/public/',
+        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
     },
     {
         name: '2',
         icon_type: 'ios-navigate',
         text: 'sub Option 2',
-        redirect_url: '/vista-framework/public/',
+        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
     },
     {
         name: '3',
         icon_type: 'ios-navigate',
         text: 'sub Option 3',
-        redirect_url: '/vista-framework/public/',
+        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
         children: [
             {
                 name: '3-1',
                 icon_type: 'ios-navigate',
                 text: 'sub Option group name 1',
-                redirect_url: '/vista-framework/public/',
+                redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                 children: [
                     {
                         name: '3-1-1',
                         icon_type: 'ios-navigate',
                         text: 'sub Option 3-1',
-                        redirect_url: '/vista-framework/public/',
+                        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                     },
                     {
                         name: '3-1-2',
                         icon_type: 'ios-navigate',
                         text: 'sub Option 3-2',
-                        redirect_url: '/vista-framework/public/',
+                        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                     },
                     {
                         name: '3-1-3',
                         icon_type: 'ios-navigate',
                         text: 'sub Option 3-3',
-                        redirect_url: '/vista-framework/public/',
+                        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                     },
                 ]
             },
@@ -47,25 +47,25 @@ const test_form_sub_menu = [
                 name: '3-2',
                 icon_type: 'ios-navigate',
                 text: 'sub Option group name 2',
-                redirect_url: '/vista-framework/public/',
+                redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                 children: [
                     {
                         name: '3-2-1',
                         icon_type: 'ios-navigate',
                         text: 'sub Option 3-4',
-                        redirect_url: '/vista-framework/public/',
+                        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                     },
                     {
                         name: '3-2-2',
                         icon_type: 'ios-navigate',
                         text: 'sub Option 3-5',
-                        redirect_url: '/vista-framework/public/',
+                        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                     },
                     {
                         name: '3-2-3',
                         icon_type: 'ios-navigate',
                         text: 'sub Option 3-6',
-                        redirect_url: '/vista-framework/public/',
+                        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
                     },
                 ]
             },
