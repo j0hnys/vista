1859bc3688434d377b36fc31184e74aca468a163
j0hnys <contact_me@johnstamkos.com>
2018-12-14 22:01:23 +0200

    Update main_menu.js

--- demo_structure/resources/js/navigation/main_menu/main_menu.js
+++ demo_structure/resources/js/navigation/main_menu/main_menu.js
@@ -3,19 +3,19 @@ const main_menu = [
         name: '1-1',
         icon_type: 'ios-navigate',
         text: 'Option 1',
-        redirect_url: '/vista-framework/public/',
+        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
     },
     {
         name: '1-2',
         icon_type: 'ios-navigate',
         text: 'Option 2',
-        redirect_url: '/vista-framework/public/',
+        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
     },
     {
         name: '1-3',
         icon_type: 'ios-navigate',
         text: 'Option 3',
-        redirect_url: '/vista-framework/public/',
+        redirect_url: process.env.MIX_BASE_RELATIVE_URL+'/',
     },
 ];
 export default main_menu;
